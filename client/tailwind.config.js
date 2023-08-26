ef53f919277857815b5b784fdac8abe79e1f0990
Hikmahx <hikmayousuph@gmail.com>
2023-08-26 05:03:43 +0100

    tailwind updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-
+      colors: {
+        primary: "#b93e90",
+      },
       fontFamily: {
         'karla': ['Karla', 'sans-serif'],
       }
