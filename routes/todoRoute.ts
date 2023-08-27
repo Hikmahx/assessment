2c93249043dc29df8ec9cbf37ff6a739b79406ce
Hikmahx <hikmayousuph@gmail.com>
2023-08-27 02:50:04 +0100

    todo backend route updated

--- routes/todoRoute.ts
+++ routes/todoRoute.ts
@@ -4,18 +4,20 @@ import {
   getTodos,
   updateTodo,
   deleteTodo,
-} from '../controllers/todoController';
+} from "../controllers/todoController";
 import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post('/todos', 
-body("todo", "Todo is required").not().isEmpty(),
-body("completed", "completed is required").not().isEmpty(),
+router.post(
+  "/",
+  body("todo", "Todo is required").not().isEmpty(),
+  body("completed", "completed is required").not().isEmpty(),
 
-createTodo);
-router.get('/todos', getTodos);
-router.put('/todos/:id', updateTodo);
-router.delete('/todos/:id', deleteTodo);
+  createTodo
+);
+router.get("/", getTodos);
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 module.exports = router;
