7c19503ff21e66c4a1bbc3bd1f9dd7ff6dab4a67
Hikmahx <hikmayousuph@gmail.com>
2023-08-28 18:36:03 +0100

    files updated

--- server.ts
+++ server.ts
@@ -1,15 +1,15 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const { connectDB } = require("./config/db");
-const path = require("path");
-const cors = require("cors");
+import express, { Request, Response } from "express"; // Import express and type definitions
+import dotenv from "dotenv";
+import connectDB from './config/db';
+import path from "path";
+import cors from "cors";
 
 dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // CORS
 app.use(cors());
